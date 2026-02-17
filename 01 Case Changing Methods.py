df6056e767ee44aa3779e947144b73aab9d78157
John Vincent Malayo <malayo13johnvincent@gmail.com>
2026-02-17 22:02:28 +0800

    Add files via upload

---
+++ 01 Case Changing Methods.py
@@ -0,0 +1,20 @@
+# Case Changing Methods
+
+text = "tHis Is a MiXeD CaSe StRiNg."
+
+
+# All upperase
+print("UPPER:", text.upper())
+
+# All lowercase
+print("lower:", text.lower())
+
+# First char uppercase
+print("Capitilize:", text.capitalize())
+
+# First char of each word uppercase
+print("Title:", text.title())
+
+# Swap upper/lower case
+print("Swapcase:", text.swapcase())
+
