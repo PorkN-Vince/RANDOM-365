60a073e90b49e7b697f00c0ef8fc7709e6836b29
John Vincent Malayo <malayo13johnvincent@gmail.com>
2026-02-20 20:09:42 +0800

    Add files via upload

---
+++ 04 Playground.py
@@ -0,0 +1,22 @@
+
+a, b = b, a
+
+reversed_s = s[::-1]
+
+is_palindrome = (s == s[::-1])
+
+import math
+factorial = math.prod(range(1, n + 1))
+
+flat_list = [i for sub in lst for i in sub]
+
+evens = [x for x in range(10) if x % 2 == 0]
+
+merged_dict = {**d1, **d2}
+
+from collections import Counter
+item_counts = Counter(lst)
+
+unique_elements = set(lst)
+
+string_joined = ' '.join(lst)
